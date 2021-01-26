d3b3e9defd187109b2ac9933cb3cb4773f0b6ed2
larissahortencio <larissa.souza@picpay.com>
2021-01-25 23:11:26 -0300

    Primeira Aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz de programação web</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p> Este é o quiz de programação web</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz</h1>
+            <p> Quiz de programação web</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+
+      <GitHubCorner />
+    </QuizBackground>
+  );
 }
